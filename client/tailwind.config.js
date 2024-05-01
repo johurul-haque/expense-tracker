572d914bdd8bef95f333570f64ec0f6324b30fec
Johurul Haque <johurulhaquejony@gmail.com>
2024-05-02 01:19:07 +0600

    added tailwind, shadcn and implemented import alias

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{ts,tsx}'],
+  prefix: '',
+  theme: {
+    container: {
+      center: true,
+      padding: '1rem',
+      screens: {
+        '2xl': '1400px',
+      },
+    },
+    extend: {},
+  },
+  plugins: [require('tailwindcss-animate')],
+};
