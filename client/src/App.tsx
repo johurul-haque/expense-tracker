ca0f3da2752d75e61d9e8d5dcce3583ba4824536
Johurul Haque <johurulhaquejony@gmail.com>
2024-05-02 11:07:08 +0600

    design changes. Added shadcn card and button component

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,27 +1,23 @@
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card';
 import { useState } from 'react';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [totalSpent, setTotalSpent] = useState(0);
 
   return (
-    <>
-      <div className="flex flex-col">
-        <button
-          className="bg-blue-100"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          up
-        </button>
-
-        <button
-          className="bg-red-100 hover:bg-red-300"
-          onClick={() => setCount((count) => count - 1)}
-        >
-          down
-        </button>
-        <p>{count}</p>
-      </div>
-    </>
+    <Card className="w-[350px] mx-auto">
+      <CardHeader>
+        <CardTitle>Total Spent</CardTitle>
+        <CardDescription>The total amount you've spent.</CardDescription>
+      </CardHeader>
+      <CardContent>{totalSpent}</CardContent>
+    </Card>
   );
 }
 
