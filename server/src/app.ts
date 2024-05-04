76288aea93ca8c652320c587ff51c144dc582257
Johurul Haque <johurulhaquejony@gmail.com>
2024-05-04 10:39:51 +0600

    implemented kinde authentication

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,13 +1,17 @@
 import { serveStatic } from '@hono/node-server/serve-static';
 import { Hono } from 'hono';
 import { logger } from 'hono/logger';
+import { authRoutes } from './routes/auth';
 import { expensesRoutes } from './routes/expenses';
 
 export const app = new Hono();
 
 app.use('*', logger());
 
-const apiRoutes = app.basePath('/api').route('/expenses', expensesRoutes);
+const apiRoutes = app
+  .basePath('/api')
+  .route('/', authRoutes)
+  .route('/expenses', expensesRoutes);
 
 app.get('*', serveStatic({ root: './client/dist' }));
 app.get('*', serveStatic({ path: './client/dist/index.html' }));
--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -4,5 +4,10 @@ import { z } from 'zod';
 export const env = z
   .object({
     PORT: z.coerce.number().default(3000),
+    KINDE_DOMAIN: z.string().url(),
+    KINDE_CLIENT_ID: z.string(),
+    KINDE_CLIENT_SECRET: z.string(),
+    KINDE_REDIRECT_URI: z.string().url(),
+    KINDE_LOGOUT_REDIRECT_URI: z.string().url(),
   })
   .parse(process.env);
